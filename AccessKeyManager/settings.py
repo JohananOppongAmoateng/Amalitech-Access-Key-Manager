a5173d1a07800d02cc2b3bbba47a3508990d0500
Johanan Oppong Amoateng <johananoppongamoateng2001@gmail.com>
2024-05-17 20:23:04 +0000

    deploy

--- AccessKeyManager/settings.py
+++ AccessKeyManager/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY","django-insecure-9z+4ci&iqo+l4jnh99zytt5y09bgb$p%e__$hy1a^4ts)iuafn")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =  True
+DEBUG =  False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["amalitech-access-key-manager.onrender.com"]
 
 
 # Application definition
