939a9bf0b9aab4df9dcdd99db289751793f6f775
JohananOppongAmoateng <johananoppongamoateng2001@gmail.com>
2024-06-24 21:25:25 +0000

    fixes

--- registration/admin.py
+++ registration/admin.py
@@ -4,12 +4,12 @@ from .models import CustomUser,ActivationToken
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .forms import SignUpForm, CustomUserChangeForm
 from .models import CustomUser
 
 
 class CustomUserAdmin(UserAdmin):
-    add_form = CustomUserCreationForm
+    add_form = SignUpForm
     form = CustomUserChangeForm
     model = CustomUser
     list_display = ("email", "is_staff", "is_active",)
