43da29158758082fc020cafa9e751022e560943a
JohananOppongAmoateng <johananoppongamoateng2001@gmail.com>
2024-06-24 21:13:06 +0000

    fixes

--- registration/forms.py
+++ registration/forms.py
@@ -1,6 +1,15 @@
-from django.contrib.auth.forms import BaseUserCreationForm
+from django.contrib.auth.forms import BaseUserCreationForm,UserChangeForm
 from .models import CustomUser
+
+
 class SignUpForm(BaseUserCreationForm):
     class Meta:
         model = CustomUser
         fields = ["email","password1","password2"]
+
+
+class CustomUserChangeForm(UserChangeForm):
+
+    class Meta:
+        model = CustomUser
+        fields = ("email",)
